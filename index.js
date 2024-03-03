046cd7245180845fce035bdefe4b8c32ef081b2f
Osama daoud <osamadaoud66@gmail.com>
2024-03-03 05:51:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Write your algorithm here 
+  for (i=0 ; i<word.length/2 ; i++){
+    const j = word.length-1-i; 
+    if(word[i]!==word[j]) return false
+
+
+  } 
+  return true
+
 }
 
 /* 
-  Add your pseudocode here
+  Add your pseudocode here 
+  I will write a function that will check if the word is plindrome and return true or false if its not 
+  at first I want to iterate on the word untill the middle .
 */
 
 /*
-  Add written explanation of your solution here
+  Add written explanation of your solution here 
+  ok 
+   
+  racecar 
+  1234567
+  0123456     
+  i     j
+
+   0123456     
+    i   j
+
+    0123456     
+      i j
+
+      0123456     
+         ij
+
+
+
+
+
+
 */
 
 // You can run `node index.js` to view these console logs
